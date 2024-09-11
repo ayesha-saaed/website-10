2093181381c6232f8a3e91a5a3fa4958e6bd06ec
Your Name <ayeshrao2004@gmail.com>
2024-09-11 09:01:37 -0700

    practice

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,11 @@
 export default function Home(){
   return(
     <>
-    <h1>Website project#10</h1>
-    </>
+    <h1>Hello everyone!</h1>
+    <h2>Shop anything from amazon website</h2>
+    <h2>Link for amazon is below   &#x21b2;</h2>
+    <a href="https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiAo_WPnruIAxUirWgJHRJHL7AYABAAGgJ3Zg&co=1&ase=2&gclid=CjwKCAjw_4S3BhAAEiwA_64YhnkCG0Zvj3xI6aW_rtkGictxD7eoHOHikEjQZbcAlvdmXMVY5mnVhxoCB2oQAvD_BwE&ohost=www.google.com&cid=CAESVuD2yv7Cgt9rIQNm0VZpu1bSSAQjrf8FJePqKiCEfx1RNUE7u17CrEsZSD-Bbr93sZjgrS3u1lfVsenx1UIHCA9jVhCHX5k4HFH02Otao0Nn11zDyw9V&sig=AOD64_2-LqqGXU0U0x2PRcptXOsfc78KkQ&q&nis=4&adurl&ved=2ahUKEwiM1--PnruIAxURdaQEHQLaMy0Q0Qx6BAgUEAE"target="_blank"> go to the Amazon</a>
+
+ </>
   );
 }
